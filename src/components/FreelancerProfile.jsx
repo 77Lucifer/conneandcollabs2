1138340de78facf8907d92ef1afcf3badfb4b4ee
77Lucifer <lucifer17860@gmail.com>
2024-03-29 19:23:25 +0530

    changes Design

--- src/components/FreelancerProfile.jsx
+++ src/components/FreelancerProfile.jsx
@@ -5,6 +5,15 @@ import { getFirestore, doc, getDoc } from "firebase/firestore";
 import Nav from "./Nav";
 import "./Style/Freelancepro.css";
 import { db } from "../firebase-config";
+import el1 from "../components/images/el1.png"
+import el2 from "../components/images/el2.png"
+import el3 from "../components/images/el3.png"
+import el4 from "../components/images/el4.png"
+import el5 from "../components/images/el5.png"
+import el6 from "../components/images/el6.png"
+import li1 from "../components/images/li2.png"
+import li2 from "../components/images/li3.png"
+import li3 from "../components/images/li4.png"
 
 const FreelancerProfile = () => {
   const [profileData, setProfileData] = useState(null);
@@ -65,19 +74,30 @@ const FreelancerProfile = () => {
       // You can also provide feedback to the user that the CV file is not available.
     }
   };
-  
+  const [isSunVisible, setIsSunVisible] = useState(true);
+
+  const toggleIcon = () => {
+      setIsSunVisible(!isSunVisible);
+  };
 
   return (
     <>
       <Nav />
-      <div className='f-profile-main'>
+      <div className={`f-profile-main ${isSunVisible ? 'night-mode' : 'day-mode'}`}>
+      {isSunVisible ? (
+        <i class="fa-regular fa-sun" onClick={toggleIcon}></i> 
+        ) : (
+          <i class="fa-regular fa-moon"  onClick={toggleIcon}></i>
+      )}
         {loading ? (
-          <p>Loading...</p>
+          <p>Loading... </p>
         ) : (
           <div className="f-user-detail">
             <div className="f-user-container">
               <div className="f-top-img">
+                <div className="block">
                 <img src={profileData && profileData.imageUrl} alt={profileData?.fullName || 'Freelancer'} />
+                </div>
                 <div className="f-name-profession">
                   <h3>{profileData && profileData.fullName}</h3>
                   <h4>{profileData?.skill || 'Profession Not Available'}</h4>
@@ -139,6 +159,15 @@ const FreelancerProfile = () => {
             </div>
           </div>
         )}
+        <img className="elem-img el1-img" src={el1} alt="" />
+        <img className="elem-img el2-img" src={el2} alt="" />
+        <img className="elem-img el3-img" src={el3} alt="" />
+        <img className="elem-img el4-img" src={el4} alt="" />
+        <img className="elem-img el5-img" src={el5} alt="" />
+        <img className="elem-img el6-img" src={el6} alt="" />
+        <img className="liel-img li1" src={li1} alt="" />
+        <img className="liel-img li2" src={li2} alt="" />
+        <img className="liel-img li3" src={li3} alt="" />
       </div>
     </>
   );
