69cdcd54fbff8c78a98ae35b97004147e31fb33b
77Lucifer <lucifer17860@gmail.com>
2024-04-08 13:38:08 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,42 @@
 // App.jsx
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Loader from './components/Loader';
-import Nav from './components/Nav';
-import Topbanner from './components/Topbanner';
-import Leftswipe from './components/Leftswipe';
-import Page2 from './components/Page2';
-import Page3 from './components/Page3';
-import Workleftswipe from './components/Workleftswipe';
-import Page4 from './components/Page4';
-import Partners from './components/Partners';
-import Footer from './components/Footer';
-import Contact from './components/Contact';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import Freelancer from './components/Freelancer';
-import PrivacyPolicy from './components/PrivacyPolicy';
-import TermsConditions from './components/TermsConditions';
-import ReturnRefund from './components/ReturnRefund';
-import FreelancerProfile from './components/FreelancerProfile';
+import './App.css'
+// import Loader from './components/Loader';
+// import Nav from './components/Nav';
+// import Topbanner from './components/Topbanner';
+// import Leftswipe from './components/Leftswipe';
+// import Page2 from './components/Page2';
+// import Page3 from './components/Page3';
+// import Workleftswipe from './components/Workleftswipe';
+// import Page4 from './components/Page4';
+// import Partners from './components/Partners';
+// import Footer from './components/Footer';
+// import Contact from './components/Contact';
+// import Signup from './components/Signup';
+// import Login from './components/Login';
+// import Freelancer from './components/Freelancer';
+// import PrivacyPolicy from './components/PrivacyPolicy';
+// import TermsConditions from './components/TermsConditions';
+// import ReturnRefund from './components/ReturnRefund';
+// import FreelancerProfile from './components/FreelancerProfile';
 
 const App = () => {
-  useEffect(() => {
-    // Scroll to the "Services" section when the component mounts
-    const servicesSection = document.getElementById('services-section');
-    if (servicesSection) {
-      servicesSection.scrollIntoView({ behavior: 'smooth' });
-    }
-    window[['CHIPBOT','DOMAIN','ID'].join('_')] = 'dom_fMzJDBSskPF-';
-        !function(c,h,i,p,b,o,t){o=h.createElement(i),t=h.getElementsByTagName(i)[0],
-          o.async=1,o.src="https://static.getchipbot.com/edge/p/chipbot.js",
-        t.parentNode.insertBefore(o,t)}(window,document,"script");
-  }, []);
+  // useEffect(() => {
+  //  
+  //   const servicesSection = document.getElementById('services-section');
+  //   if (servicesSection) {
+  //     servicesSection.scrollIntoView({ behavior: 'smooth' });
+  //   }
+  //   window[['CHIPBOT','DOMAIN','ID'].join('_')] = 'dom_fMzJDBSskPF-';
+  //       !function(c,h,i,p,b,o,t){o=h.createElement(i),t=h.getElementsByTagName(i)[0],
+  //         o.async=1,o.src="https://static.getchipbot.com/edge/p/chipbot.js",
+  //       t.parentNode.insertBefore(o,t)}(window,document,"script");
+  // }, []);
 
   return (
     <>
-      <Loader />
+      {/* <Loader />
       <Nav />
       <Topbanner />
       <Leftswipe />
@@ -44,10 +45,10 @@ const App = () => {
       <Workleftswipe />
       <Page4 />
       <Partners />
-      <Footer />
+      <Footer /> */}
 
       <Routes>
-        <Route path="/Freelancer" element={<Freelancer />} />
+        {/* <Route path="/Freelancer" element={<Freelancer />} />
         <Route path="/Freelancer/:username" element={<FreelancerProfile />} />
 
         <Route path="/Contact" element={<Contact />} />
@@ -55,8 +56,12 @@ const App = () => {
         <Route path="/Login" element={<Login />} />
         <Route path="/privacypolicy" element={<PrivacyPolicy />} />
         <Route path="/termsconditions" element={<TermsConditions />} />
-        <Route path="/returnrefund" element={<ReturnRefund />} />
+        <Route path="/returnrefund" element={<ReturnRefund />} /> */}
       </Routes>
+      <div className="temp-div">
+      <h1>Talk To me about my salary </h1>
+        <h2>Then This website will work . . . . . . . .</h2>
+      </div>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,54 +6,54 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // Import createBrowserRouter and RouterProvider
 import App from './App.jsx';
-import Freelancer from './components/Freelancer.jsx';
-import Contact from './components/Contact.jsx';
-import Signup from './components/Signup.jsx';
-import Login from './components/Login.jsx';
-import PrivacyPolicy from './components/PrivacyPolicy.jsx';
-import TermsConditions from './components/TermsConditions.jsx';
-import ReturnRefund from './components/ReturnRefund.jsx';
-import './index.css'
-import FreelancerProfile from './components/FreelancerProfile.jsx';
+// import Freelancer from './components/Freelancer.jsx';
+// import Contact from './components/Contact.jsx';
+// import Signup from './components/Signup.jsx';
+// import Login from './components/Login.jsx';
+// import PrivacyPolicy from './components/PrivacyPolicy.jsx';
+// import TermsConditions from './components/TermsConditions.jsx';
+// import ReturnRefund from './components/ReturnRefund.jsx';
+// import './index.css'
+// import FreelancerProfile from './components/FreelancerProfile.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
   },
-  {
-    path: '/Freelancer',
-    element: <Freelancer />,
-  },
-  {
-    path: '/Freelancer/:id',
-    element: <FreelancerProfile />,
-  },
+  // {
+  //   path: '/Freelancer',
+  //   element: <Freelancer />,
+  // },
+  // {
+  //   path: '/Freelancer/:id',
+  //   element: <FreelancerProfile />,
+  // },
 
-  {
-    path: '/Contact',
-    element: <Contact />,
-  },
-  {
-    path: '/Signup',
-    element: <Signup />,
-  },
-  {
-    path: '/Login',
-    element: <Login />,
-  },
-  {
-    path: '/privacypolicy',
-    element: <PrivacyPolicy />,
-  },
-  {
-    path: '/termsconditions',
-    element: <TermsConditions />,
-  },
-  {
-    path: '/returnrefund',
-    element: <ReturnRefund />,
-  },
+  // {
+  //   path: '/Contact',
+  //   element: <Contact />,
+  // },
+  // {
+  //   path: '/Signup',
+  //   element: <Signup />,
+  // },
+  // {
+  //   path: '/Login',
+  //   element: <Login />,
+  // },
+  // {
+  //   path: '/privacypolicy',
+  //   element: <PrivacyPolicy />,
+  // },
+  // {
+  //   path: '/termsconditions',
+  //   element: <TermsConditions />,
+  // },
+  // {
+  //   path: '/returnrefund',
+  //   element: <ReturnRefund />,
+  // },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -61,14 +61,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <RouterProvider router={router}>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/Freelancer" element={<Freelancer />} />
+          {/* <Route path="/Freelancer" element={<Freelancer />} />
           <Route path="/Freelancer/:username" component={FreelancerProfile} />
           <Route path="/Contact" element={<Contact />} />
           <Route path="/Signup" element={<Signup />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/privacypolicy" element={<PrivacyPolicy />} />
           <Route path="/termsconditions" element={<TermsConditions />} />
-          <Route path="/returnrefund" element={<ReturnRefund />} />
+          <Route path="/returnrefund" element={<ReturnRefund />} /> */}
 
         </Routes>
       </RouterProvider>
